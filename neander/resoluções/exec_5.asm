c67309c13c6d2b1318daac3c202f79ef670d1ffb
henryzord <henry.cagnini@gmail.com>
2022-07-12 20:21:22 -0300

    fixed #1

--- neander/resoluções/exec_5.asm
+++ neander/resoluções/exec_5.asm
@@ -3,24 +3,30 @@
 ; Autor: Henry Cagnini
 ; Data: 27/06/22
 ;---------------------------------------------------
+
+; primeiro operando está na posição 128_10 ou 80_16 de memória
+; segundo operando está na posição 129_10 ou 81_16 de memória
+
 ; prepara a memória para receber o resultado do valor da multiplicação
 LDI 0
 STA 132
 ; prepara o número 1 na memória, para subtrair do segundo operando
 LDI 1    
-STA 130  
-; verificar se o primeiro operando é zero. se for, nem multiplica números
-LDA 128  
-JZ 38    
-STA 132
-; verificar se o segundo operando é zero. 
-; se não for, subtrai 1 e armazena em outra posição da memória, pois começamos
-; a contar a partir do zero
-; se o segundo operando for zero, nem multiplica números
+STA 130
+; verifica se o segundo operando é zero. 
+; se for, nem multiplica números e mostra zero na tela
+; se não for, subtrai 1 e armazena em outra posição da memória, 
+; pois começamos a contar a partir do zero
 LDA 129  
 JZ 38    
 SUB 130
-STA 134  
+STA 134    
+; verificar se o primeiro operando é zero. 
+; se for, nem multiplica números
+; se não for, armazena-o em outra posição da memória
+LDA 128  
+JZ 38
+STA 132
 ; início do for loop
 LDA 132
 ADD 128
